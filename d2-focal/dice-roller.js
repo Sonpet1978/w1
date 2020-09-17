16bfcc6c11973c92c75c6b4b8ff87821ce230dcc
Sonya Korolev Shapiro <sonyakorolevshapiro@Sonyas-MacBook-Air.local>
2020-09-17 09:48:05 -0400

    add

--- d2-focal/dice-roller.js
+++ d2-focal/dice-roller.js
@@ -1,10 +1,13 @@
-function diceRoller (){
-    var die1;   // The number on the first die.
-    var die2;   // The number on the second die.
-    var roll;   // The total roll (sum of the two dice).
-        
-        die1 = (int)(Math.random()*6) + 1;
-        die2 = (int)(Math.random()*6) + 1;
-        roll = die1 + die2;
-  console.log(roll);
+const person = "Martha";
+
+// BAD
+const sayHelloBadly = function() {
+  console.log(`Howdy, ${person}`);
 }
+sayHelloBadly(); // Works, but not ideal!
+
+// GOOD
+const sayHelloGoodly = function(name) {
+  console.log(`Howdy, ${name}`);
+}
+sayHelloGoodly(person);
