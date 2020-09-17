5b00e40e49b6828a04725eddd4e524ae61d87d9d
Sonya Korolev Shapiro <sonyakorolevshapiro@Sonyas-MacBook-Air.local>
2020-09-17 09:54:17 -0400

    fixes

--- d2-focal/reverse.js
+++ d2-focal/reverse.js
@@ -1,8 +1,9 @@
+function reverse(original) {
+  return original.split('').reverseList().join('');
+}
 var input = process.argv[2];
 
 if (input)
   console.log(reverse(input));
 
-function reverse(original) {
-  return orignal.split('').reverseList().join('');
-}
+
