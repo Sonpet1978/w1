6cc732d097f9f3a86f98d24e8c6f5c289693849d
Sonya Korolev Shapiro <sonyakorolevshapiro@Sonyas-MacBook-Air.local>
2020-09-19 12:19:55 -0400

    add car test

---
+++ we/car/test/carTest.js
@@ -0,0 +1,17 @@
+const chai = require('chai'); // 1
+const assert = chai.assert;
+
+const shouldBuyCar = require('../shouldBuyCar.js'); // 2
+
+describe("#shouldBuyCar()", function() { // 3
+
+  it("should return false if it's a hatchback", function() { // 4
+    const car = {
+        type: "hatchback"
+      };
+      const shouldBuy = shouldBuyCar(car);
+      assert.isFalse(shouldBuy);
+
+  });
+
+});
