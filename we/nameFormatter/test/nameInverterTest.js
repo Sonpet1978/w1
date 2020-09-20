2742b1268f9521d6d59afe4139aabc3e8e197be6
Sonya Korolev Shapiro <sonyakorolevshapiro@Sonyas-MacBook-Air.local>
2020-09-20 09:18:54 -0400

    add

--- we/nameFormatter/test/nameInverterTest.js
+++ we/nameFormatter/test/nameInverterTest.js
@@ -4,5 +4,10 @@ const assert = chai.assert;
 const nameInverter = require('../nameInverter');
 
 describe('nameInverter', function() {
+    it('should return an empty string when passed an empty string', function() {
+        const inputName = "";
+        const expectedOutput = "";
+        assert.equal(nameInverter(inputName), expectedOutput);
+      });
 
 });
